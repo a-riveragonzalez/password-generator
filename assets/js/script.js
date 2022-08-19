10bcfe94e3d291550e41ec201e055f7f9dd10073
Arely Rivera Gonzalez <arelyrivera1996@gmail.com>
2022-08-19 00:17:12 -0700

    added if statements to capture user inputs and store them in an array

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,7 @@ var specialChar = ["!","\u0022","#","$","%","&","'","(",")","*","+",",","-",
   ".","/",":",";","<","=",">","?","@","[","\u005C","]","^","_","`","{",
   "|", "}","~",];
 
-console.log(specialChar);
+var masterArray = []; //this is where we will push other arrays
 
 function generatePassword(){
   var userLength = prompt("How many characters do you want your password? \n Pick a number between 8 and 128");
@@ -18,6 +18,36 @@ function generatePassword(){
   var userNumbers = confirm("Would you like numbers? \n Press ok for yes. Press cancel for no.");
   var userSpecial = confirm("Would you like special characters? \n Press ok for yes. Press cancel for no.");
 
+  // error control 
+  if (!userLower && !userUpper && !userNumbers && !userSpecial){
+    alert("You need to select at least one type of character to create a password");
+    return generatePassword();
+  }
+
+  // pushing user choices into the master array
+  if (userLower){
+    for (var i = 0; i < lowerCase.length; i++){
+      masterArray.push(lowerCase[i]);
+    }
+  }
+
+  if (userUpper){
+    for (var i = 0; i < upperCase.length; i++){
+      masterArray.push(upperCase[i]);
+    }
+  }
+
+  if (userNumbers){
+    for(var i = 0; i < numbers.length; i++){
+      masterArray.push(numbers[i]);
+    }
+  }
+
+  if (userSpecial){
+    for(var i = 0; i < specialChar.length; i++){
+      masterArray.push(specialChar[i]);
+    }
+  }
 }
 
 // Get references to the #generate element
@@ -34,4 +64,5 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+console.log(masterArray);
 
